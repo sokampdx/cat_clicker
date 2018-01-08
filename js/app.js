bdf41e915053093eb598dc274f9a7a966bd0dfeb
Anthony RTK <sokasoka@hotmail.com>
2018-01-08 15:16:33 -0800

    2 cat clicker

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
-var clicker = document.getElementById('clicker');
-var counter = document.getElementById('counter');
-var count = parseInt(counter.textContent);
+var clicker0 = document.getElementById('clicker0');
+var counter0 = document.getElementById('counter0');
+var clicker1 = document.getElementById('clicker1');
+var counter1 = document.getElementById('counter1');
+var count0 = counter0.textContent;
+var count1 = counter1.textContent;
 
-clicker.onclick = function() {
-  count = count + 1;
-  counter.textContent = count
-  console.log('counter' + counter);
+clicker0.onclick = function() {
+  count0++;
+  counter0.textContent = count0;
+  console.log('counter0' + counter0);
+};
+
+clicker1.onclick = function() {
+  count1++;
+  counter1.textContent = count1;
+  console.log('counter1' + counter1);
 };
